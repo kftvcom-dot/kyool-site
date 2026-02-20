48e750ece0bd445b826100ff272574cba457f5ef
kftvcom-dot <kftv.com@gmail.com>
2026-02-21 00:16:35 +0900

    Update programmes-categorie.js

--- programmes/programmes-categorie.js
+++ programmes/programmes-categorie.js
@@ -124,7 +124,7 @@ async function loadCategorie() {
   document.getElementById('breadcrumbTitle').textContent = currentCategorie.title;
   document.getElementById('categorieTitle').textContent = currentCategorie.title;
   document.getElementById('categorieDescription').textContent = currentCategorie.description;
-  if (catSlug === 'documentaire' || catSlug === 'tv-shows') {
+  if (catSlug === 'documentaire' || catSlug === 'kyool-original' || catSlug === 'tv-shows') {
   document.body.classList.add('category-' + catSlug);
 }
   // Afficher le loader
